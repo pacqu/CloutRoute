2e2d99d05b0b991803c25ac6b452fba69ebfae90
Matthew Binshtok <matthew.binshtok@gmail.com>
2018-06-19 14:05:09 -0400

    add page structure

--- cloutroute/src/components/Login.js
+++ cloutroute/src/components/Login.js
@@ -2,23 +2,27 @@ import React, { Component } from 'react';
 import './Login.css';
 
 class Login extends Component {
-  render() {
-    return (
-      <div className="login">
-          <div className="logo-container">
 
-          </div>
-          <div className="input-container">
-               <div className="username-input-container">
 
+
+     render() {
+          return (
+               <div className="login">
+                    <div className="logo-container">
+                         <img src="../../graphics/clout_route_logo-01.png"> </img>
+                    </div>
+                    <div className="input-container">
+                         <div className="username-input-container">
+                              <input id="username-entry" type="text"> </input>
+                         </div>
+                         <div className="password-input-container">
+                              <input id="password-entry" type="password"> </input>
+                         </div>
+                    </div>
                </div>
-               <div className="password-input-container">
+          );
+     }
 
-               </div>
-          </div>
-      </div>
-    );
-  }
 }
 
 export default Login;
