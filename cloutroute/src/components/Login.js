0b1148a4b3f96978f0441d87ecf73d339b47233c
Matthew Binshtok <matthew.binshtok@gmail.com>
2018-06-19 11:59:57 -0400

    add render skeleton for Login

--- cloutroute/src/components/Login.js
+++ cloutroute/src/components/Login.js
@@ -4,7 +4,18 @@ import './Login.css';
 class Login extends Component {
   render() {
     return (
-      <div className="Login">
+      <div className="login">
+          <div className="logo-container">
+
+          </div>
+          <div className="input-container">
+               <div className="username-input-container">
+
+               </div>
+               <div className="password-input-container">
+
+               </div>
+          </div>
       </div>
     );
   }
