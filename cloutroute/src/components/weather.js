eadfe5595a6e54a2e51a8aad3ef0f9849667255b
nsohn <noam.sohn63@myhunter.cuny.edu>
2018-06-19 14:57:31 -0400

    ability to determine if clear sky or raining

--- cloutroute/src/components/weather.js
+++ cloutroute/src/components/weather.js
@@ -5,10 +5,13 @@ const WeatherBar = ({weather}) => {
   if (!weather){
     return <div> Loading...</div>
   }
-  const min = (weather.temp_min * 1) * 9/5 - 459.67;
-  const max = (weather.temp_max * 1) * 9/5 - 459.67;
+  const min = (weather.main.temp_min * 1) * 9/5 - 459.67;
+  const max = (weather.main.temp_max * 1) * 9/5 - 459.67;
+  const percep = weather.weather[0].main;
   return (
     <div>
+      Today it is
+      {(percep === 'Clear' || percep ==='Clouds') ?  <div>CLEAR</div> : <div>RAINING</div> }
       The min is {min.toFixed(2)}.
       The max is {max.toFixed(2)}.
     </div>
