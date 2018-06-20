c69a8c44f68f7a368583bc140f23a3551b23c959
Matthew Binshtok <matthew.binshtok@gmail.com>
2018-06-20 10:36:41 -0400

    update matthew branch

--- cloutroute/src/App.js
+++ cloutroute/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import WeatherBar from './components/Weather';
+import axios from "axios";
+// import ReactWeather from 'react-open-weather';
+// import 'react-open-weather/lib/css/ReactWeather.css';
 
+const WEATHER_API_KEY = 'cfd81373d0a942ac745fc27d11206173';
+
+const country = ",us"
+const city = "Port Chester"
+
+let weather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + country + "&appid=" + WEATHER_API_KEY;
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {};
+  }
+
+  /* Calls the Weather API and sets state to
+  the data object.
+  */
+  componentDidMount(){
+    console.log("weather", weather);
+    axios.get(weather)
+    .then(res => {
+      this.setState({
+        // weather:res.data.main
+        weather: res.data
+      })
+      console.log("response", res)
+    });
+  }
+
   render() {
+   const weather = this.state.weather;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <WeatherBar weather={weather} />
       </div>
     );
   }
