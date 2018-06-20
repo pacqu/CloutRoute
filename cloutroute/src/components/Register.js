2e119868bb17dcb2e4fec4c510f44b52138634a7
Matthew Binshtok <matthew.binshtok@gmail.com>
2018-06-20 15:29:28 -0400

    add backend integration for Register component

--- cloutroute/src/components/Register.js
+++ cloutroute/src/components/Register.js
@@ -9,6 +9,20 @@ class Register extends Component {
           console.log("Username: " + this.refs.username_entry.value);
           console.log("Password: " + this.refs.password_entry.value);
           console.log("City: " + this.refs.city_entry.value);
+          var jsonBody = {
+            'username': this.refs.username_entry.value,
+            'password': this.refs.password_entry.value,
+            'city': this.refs.city_entry.value
+          };
+          console.log(jsonBody);
+          fetch('/users/newuser', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(jsonBody)
+          }).then((res) => res.json()).then((data) =>  console.log(data));
      }
 
      render() {
