0939c45e3cf72cd9201106b5fd841e40b541a37f
nsohn <noam.sohn63@myhunter.cuny.edu>
2018-06-21 14:40:22 -0400

    src/components/Weather.js
    changed weather component to take city and make API call

--- cloutroute/src/components/Weather.js
+++ cloutroute/src/components/Weather.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import ReactWeather from 'react-open-weather';
 import './Weather.css';
 import './weather-icons-master/css/weather-icons.css';
 
@@ -10,53 +11,50 @@ the low and high, and based on the precipitation
 indicate which icon should be shown
 NEED TO ADD CSS ICONS
 */
-const WEATHER_API_KEY = 'cfd81373d0a942ac745fc27d11206173';
-class WeatherBar extends Component{
-  //the state will contain a weather object returned from API
+
+
+ class WeatherBar extends Component{
   constructor(props){
     super(props);
     this.state = {
-      city: '',
-      country = ',us'
+      country: ',us',
+      city: props.city
     }
+
   }
-  //BELOW COPIED FROM APP.JS
-  let weather = "https://api.openweathermap.org/data/2.5/weather?q=" + this.state.city + this.state.country + "&appid=" + WEATHER_API_KEY;
   componentDidMount(){
-    console.log("weather", weather);
+    // console.log("weather", weather);
+    const WEATHER_API_KEY = 'cfd81373d0a942ac745fc27d11206173';
+    let weather = "https://api.openweathermap.org/data/2.5/weather?q=" + this.state.city + this.state.country+ "&appid=" + WEATHER_API_KEY;
     axios.get(weather)
     .then(res => {
       this.setState({
         weather: res.data
       })
-      console.log("response", res)
+       console.log("response", res)
     });
   }
 
-
-  if (!weather){
-    return <div> Loading...</div>
+  render(){
+   const weather = (this.state.weather)? this.state.weather : null ;
+   const max = (weather)? this.state.weather.main.temp_max : ""
+   const min = (weather)? this.state.weather.main.temp_min : ""
+   console.log("weather", weather)
+    return (
+      <div className="Weather">
+        <div className="Weather-header">
+            Expected Minimum: {((min * 1) * 9/5 - 459.67).toFixed(2)}
+            <br />
+            Expected Maximum: {((max * 1) * 9/5 - 459.67).toFixed(2)}
+            <br />
+          {/*  <i className={
+              (precip ==='Thunderstorm' || precip === 'Drizzle' || precip==='Snow')?
+               "wi wi-umbrella" : "wi wi-forecast-io-clear-day:before"}>
+            </i>*/}
+          </div>
+        </div>
+    );
   }
-  const min = (weather.main.temp_min * 1) * 9/5 - 459.67;
-  const max = (weather.main.temp_max * 1) * 9/5 - 459.67;
-  const precip = weather.weather[0].main;
-  console.log("inside weather: ",weather);
-
-  return (
-    <div className="Weather">
-      <div className="Weather-header">
-        Expected Minimum: {min.toFixed(2)}
-        <br />
-        Expected Maximum: {max.toFixed(2)}
-        <br />
-        <i className={
-          (precip ==='Clear' || precip==='Clouds')?
-          "wi wi-umbrella" : "wi wi-forecast-io-clear-day:before"}>
-        </i>
-      </div>
-    </div>
-
-  )
 }
 
 export default WeatherBar;
