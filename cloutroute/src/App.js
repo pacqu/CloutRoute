e0f124a9ce137ccc48df76be37f37c4632b92d3d
nsohn <noam.sohn63@myhunter.cuny.edu>
2018-06-21 15:07:39 -0400

    removed weather from Appjs and preparing for master pull

--- cloutroute/src/App.js
+++ cloutroute/src/App.js
@@ -5,41 +5,21 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Entry from './components/Entry';
 import axios from "axios";
-// import ReactWeather from 'react-open-weather';
-// import 'react-open-weather/lib/css/ReactWeather.css';
-
-const WEATHER_API_KEY = 'cfd81373d0a942ac745fc27d11206173';
-const country = ",us"
-const city = "Port Chester"
-let weather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + country + "&appid=" + WEATHER_API_KEY;
+import RouteForm from './components/RouteForm';
 
 class App extends Component {
   constructor(){
     super();
-    this.state = {};
-  }
-
-  /* Calls the Weather API and sets state to
-  the data object.
-  */
-  componentDidMount(){
-    console.log("weather", weather);
-    axios.get(weather)
-    .then(res => {
-      this.setState({
-        // weather:res.data.main
-        weather: res.data
-      })
-      console.log("response", res)
-    });
+    this.state = {city: 'Port Chester'};
   }
 
   render() {
-   const weather = this.state.weather;
     return (
       <div className="App">
-        <Entry />
-        {/*<WeatherBar weather={weather} />*/}
+        {/*<RouteForm />*/}
+        {/*<Entry />*/}
+        <WeatherBar city={this.state.city}/>
+        <RouteForm />
       </div>
     );
   }
